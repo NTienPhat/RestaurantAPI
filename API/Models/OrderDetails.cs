8112fbe751733130448dbe4d802838ae0a337905
NTPhatFPT <phatntse150133@fpt.edu.vn>
2023-07-17 01:38:31 +0700

    add order and orderdetails to db

---
+++ API/Models/OrderDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int OrderDetailsId { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [Required]
+        public int MenuItemId { get; set; }
+        [ForeignKey("MenuItemId")]
+        public MenuItem MenuItem { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
